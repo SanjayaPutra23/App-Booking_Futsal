cc9c946a5caec72d5586b35c3dcd9dc5c4ea763a
Sanjaya_023 <sanjaya20004@gmail.com>
2025-05-25 11:20:41 +0700

    Update Build

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,9 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/App-Booking_Futsal/",
   plugins: [react()],
   optimizeDeps: {
-    exclude: ['lucide-react'],
+    exclude: ["lucide-react"],
   },
 });
